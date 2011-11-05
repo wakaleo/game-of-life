a8e328e28112a4220cf3f61d7c96ebee04a9887e
John Ferguson Smart <john.smart@wakaleo.com>
2011-11-06 10:25:27 +1300

    Fixed Windows-specific issues

---
+++ gameoflife-web/src/test/java/com/wakaleo/gameoflife/web/controllers/WhenSpawningANewGeneration.java
@@ -0,0 +1,33 @@
+package com.wakaleo.gameoflife.web.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.mock;
+
+
+public class WhenSpawningANewGeneration {
+
+	@Test
+	public void whenGeneratingTheNextGenerationAnUpdatedUniverseShouldBeProduced() {
+		HttpServletRequest request = mock(HttpServletRequest.class);
+		request.setAttribute("rows", "3");
+		request.setAttribute("columns", "3");
+		GameController controller = new GameController();
+		ModelAndView nextView = controller.nextGeneration(3, 3, request);	
+		
+		assertThat(nextView.getModel().get("universe"), is(not(nullValue())));
+	}
+	
+	@Test
+	public void theIndexUrlShouldDisplayTheHomePage() {
+		HomePageController controller = new HomePageController();
+		ModelAndView homeView = controller.index();
+		assertThat(homeView.getViewName(), is("home"));
+	}
+}	
+
