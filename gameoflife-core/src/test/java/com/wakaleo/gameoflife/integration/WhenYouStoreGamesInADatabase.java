c0a67fcaeba07ac154aab47a6c5320ab24e985bb
John Ferguson Smart <john.smart@wakaleo.com>
2012-09-28 07:18:42 +1000

    Simplified modules for CD demo

---
+++ gameoflife-core/src/test/java/com/wakaleo/gameoflife/integration/WhenYouStoreGamesInADatabase.java
@@ -0,0 +1,16 @@
+package com.wakaleo.gameoflife.integration;
+
+import org.junit.Test;
+
+public class WhenYouStoreGamesInADatabase {
+
+    @Test
+    public void should_store_games() {
+
+    }
+
+    @Test
+    public void should_load_saved_games() {
+    }
+
+}
