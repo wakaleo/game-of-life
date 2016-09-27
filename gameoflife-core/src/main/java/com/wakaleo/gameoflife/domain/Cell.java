5eee1717b176875aae791d6237316908a731b940
carlos.rivera11 <cart922@gmail.com>
2016-09-27 11:45:11 -0500

    Restaurar version

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,6 +1,6 @@
 package com.wakaleo.gameoflife.domain;
 public enum Cell {
-LIVE_CELL("+"), DEAD_CELL(".");
+LIVE_CELL("*"), DEAD_CELL(".");
 private String symbol;
 private Cell(String symbol) {
 this.symbol = symbol;
