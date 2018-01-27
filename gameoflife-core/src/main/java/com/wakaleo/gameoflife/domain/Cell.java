af6563c4ae30b9ff8cebfe6b747ffbbf5a860ee1
Gerald Scott <geraldo@ci00.taung.co.za>
2018-01-27 09:52:36 +0200

    Change live cell to @ and dead cell :

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -9,7 +9,7 @@ package com.wakaleo.gameoflife.domain;
  */
 public enum Cell {
 	// Symbols to represent cell status
-    LIVE_CELL("*"), DEAD_CELL(".");
+    LIVE_CELL("@"), DEAD_CELL(":");
 
     private String symbol;
 
