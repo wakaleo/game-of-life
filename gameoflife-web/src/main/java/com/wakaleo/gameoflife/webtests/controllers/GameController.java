8aba2d13ad4247d6d33db0bc4cf3b52058381428
vishwasredefined <vishwasredefined@gmail.com>
2020-04-22 18:36:07 +0530

    New commit added

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -17,7 +17,7 @@ import com.wakaleo.gameoflife.domain.Universe;
  *
  * GameController.java
  * Controller to navigate the buttons on the game pages
- */
+ *New Comment/
 @Controller
 @RequestMapping("/game")
 public class GameController {
