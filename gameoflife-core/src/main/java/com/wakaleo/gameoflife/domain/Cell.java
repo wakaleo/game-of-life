39bfc8f84925524ca95b097e8905bda7fb82ce47
charraki3 <60981619+charraki3@users.noreply.github.com>
2020-08-18 19:10:37 +0200

    Update Cell.java

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -3,7 +3,7 @@ package com.wakaleo.gameoflife.domain;
  /**
  * Wakaleo Consulting - John Ferguson Smart
  * Game of life, demonstration application for Jenkins: The Definitive Guide
- *
+ * just for tes
  * Cell.java
  * Cell object class, contains information on individual cells (positions) in the grid
  */
