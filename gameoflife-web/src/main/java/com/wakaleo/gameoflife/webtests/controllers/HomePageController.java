3d6ec8273754e7cce49a019d56eff52f150d9a56
Gopi757 <61792836+Gopi757@users.noreply.github.com>
2020-12-26 08:56:29 -0500

    Update HomePageController.java

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/HomePageController.java
@@ -10,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
  *
  * HomePageController.java
  * Controller to navigate to the main page
+ * commit adding for testing
  */
 @Controller
 public class HomePageController {
