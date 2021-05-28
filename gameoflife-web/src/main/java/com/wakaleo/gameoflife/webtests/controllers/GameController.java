e2aa01160939147b1ef10bf9405e5961e17dca5d
rajivragnar <84559193+rajivragnar@users.noreply.github.com>
2021-05-28 15:35:00 +0530

    Update GameController.java

--- gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
+++ gameoflife-web/src/main/java/com/wakaleo/gameoflife/webtests/controllers/GameController.java
@@ -17,6 +17,7 @@ import com.wakaleo.gameoflife.domain.Universe;
  *
  * GameController.java
  * Controller to navigate the buttons on the game pages
+ * new changes for test
  */
 @Controller
 @RequestMapping("/game")
